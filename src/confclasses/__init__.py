6cc70d04fe72a139b41a0eb3c2052d216e10c5fa
Simon Fletcher <simon.fletcher@lexisnexisrisk.com>
2025-06-25 16:32:54 +0100

    vbump

--- src/confclasses/__init__.py
+++ src/confclasses/__init__.py
@@ -9,7 +9,7 @@ import functools
 import inspect
 from confclasses.exceptions import ConfclassesLoadingError, ConfclassesSetupError, ConfclassesAttributeError, ConfclassesMissingValueError
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     'configclass',
